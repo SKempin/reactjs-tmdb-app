246ed37e2e2ece1d102f3be4f3f16bc99b49fde2
Stephen Kempin <info@stephenkempin.co.uk>
2017-04-13 19:04:49 +0100

    Fixing Safari input issues / Background gradient stretch

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -17,9 +17,9 @@ class SearchBox extends Component {
           </div>
           <div className="col-xs-12 col-sm-6 col-lg-7">
             <form className="searchbox">
-              <label>
+              {/* <label> */}
                 <input ref="search suggestion" onClick={this.handleChange} className="searchbox__input typeahead form-control" type="text" placeholder="Search Movie Title..." id="q" />
-              </label>
+              {/* </label> */}
               </form>
           </div>
         </div>
