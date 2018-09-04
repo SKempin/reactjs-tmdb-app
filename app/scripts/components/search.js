290276c15e3525723b7a74678f331e7b77ed3d2f
Stephen Kempin <info@stephenkempin.co.uk>
2018-09-04 17:36:54 +0100

    Fixing mising TMDB logo - making local asset. Adding Google Play Store credential.

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-const TMDBLogo = 'https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+const TMDBLogo = "../images/tmdb.svg";
 
 class SearchBox extends Component {
-
   handleChange(event) {
     event.target.select();
   }
@@ -11,20 +10,28 @@ class SearchBox extends Component {
     return (
       <div className="col-xs-12 search-container nopadding">
         <div className="row">
-
           <div className="col-xs-12 col-sm-6 col-lg-5">
-            <a href="./" title="ReactJS TMDb Movie Search"><img src={TMDBLogo} className="logo" alt="The Movie Database" /></a>
+            <a href="./" title="ReactJS TMDb Movie Search">
+              <img src={TMDBLogo} className="logo" alt="The Movie Database" />
+            </a>
           </div>
           <div className="col-xs-12 col-sm-6 col-lg-7">
             <form className="searchbox">
               {/* <label> */}
-                <input ref="search suggestion" onClick={this.handleChange} className="searchbox__input typeahead form-control" type="text" placeholder="Search Movie Title..." id="q" />
+              <input
+                ref="search suggestion"
+                onClick={this.handleChange}
+                className="searchbox__input typeahead form-control"
+                type="text"
+                placeholder="Search Movie Title..."
+                id="q"
+              />
               {/* </label> */}
-              </form>
+            </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 module.exports = SearchBox;
